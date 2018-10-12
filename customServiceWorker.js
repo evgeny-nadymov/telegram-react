8b44a15b90f3b394c6277334617549ba6a1f26da
evgeny-nadymov <johnnypmpu@gmail.com>
2018-10-12 15:18:45 +0300

    Updates

--- customServiceWorker.js
+++ customServiceWorker.js
@@ -2,12 +2,12 @@ self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-    // const title = 'Push Codelab';
-    // const options = {
-    //     body: 'Yay it works.',
-    //     icon: 'images/icon.png',
-    //     badge: 'images/badge.png'
-    // };
-    //
-    // event.waitUntil(self.registration.showNotification(title, options));
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
 });
