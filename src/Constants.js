90069e8af651f27d4a50f407167f756ef94b39e0
evgeny-nadymov <johnnypmpu@gmail.com>
2018-10-15 08:37:16 +0300

    Updated copyright

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2018-present, Evgeny Nadymov
+ *
+ * This source code is licensed under the GPL v.3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export const PHOTO_SIZE = 320;
 export const PHOTO_DISPLAY_SIZE = 260;
 export const MESSAGE_SLICE_LIMIT = 20;
