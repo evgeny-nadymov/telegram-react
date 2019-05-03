45054f60fd92a24bbae061e95d101fa8fa57316b
evgeny-nadymov <johnnypmpu@gmail.com>
2019-05-03 11:00:21 +0300

    Updates

---
+++ precache-manifest.67c0b155a56f087e6a580d435dcd6dda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd96a1bf31d47f18bf27",
+    "url": "/telegram-react/static/css/main.343a1c9a.chunk.css"
+  },
+  {
+    "revision": "dd96a1bf31d47f18bf27",
+    "url": "/telegram-react/static/js/main.bfd9e164.chunk.js"
+  },
+  {
+    "revision": "432c3b8008689d4f3080",
+    "url": "/telegram-react/static/js/runtime~main.d23f7ed6.js"
+  },
+  {
+    "revision": "b225af945343fc4c2381",
+    "url": "/telegram-react/static/js/2.188a8283.chunk.js"
+  },
+  {
+    "revision": "4964c9bbfba510f495319c52562d70d4",
+    "url": "/telegram-react/static/media/Telegram.4964c9bb.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "5270d71cd78fd282a3736b0e6ae7f048",
+    "url": "/telegram-react/static/media/General_2x.5270d71c.png"
+  },
+  {
+    "revision": "f01cec9bccc80a678ed0ec04acecade2",
+    "url": "/telegram-react/static/media/IconsetW_2x.f01cec9b.png"
+  },
+  {
+    "revision": "7a71629a5e4f7482b6320b103f1d543c",
+    "url": "/telegram-react/static/media/Manytabs_2x.7a71629a.png"
+  },
+  {
+    "revision": "7e34f8727ba79d74bf0739853d22049c",
+    "url": "/telegram-react/index.html"
+  }
+];
