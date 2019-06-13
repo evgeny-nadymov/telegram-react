c7a5c5c58019365c47a6827bc55b17b9f92b5590
evgeny-nadymov <johnnypmpu@gmail.com>
2019-06-14 02:22:10 +0300

    Updates

---
+++ precache-manifest.d1599e7547c3fb5d2e57509d41e1f89a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "953943977c5521c49d2e",
+    "url": "/telegram-react/static/css/main.f0cc6d71.chunk.css"
+  },
+  {
+    "revision": "953943977c5521c49d2e",
+    "url": "/telegram-react/static/js/main.4e9becb1.chunk.js"
+  },
+  {
+    "revision": "2748de1390634dad7eae",
+    "url": "/telegram-react/static/js/runtime~main.47aa4457.js"
+  },
+  {
+    "revision": "9ba0e3b1fb585723164b",
+    "url": "/telegram-react/static/js/2.4beb266e.chunk.js"
+  },
+  {
+    "revision": "c85ef689bfc92bda858d",
+    "url": "/telegram-react/static/css/3.a1ca79d1.chunk.css"
+  },
+  {
+    "revision": "c85ef689bfc92bda858d",
+    "url": "/telegram-react/static/js/3.cff61a2c.chunk.js"
+  },
+  {
+    "revision": "fe83b7f2af02192a2077",
+    "url": "/telegram-react/static/js/4.ea6238bc.chunk.js"
+  },
+  {
+    "revision": "e66503c4022d4e8dd4d3",
+    "url": "/telegram-react/static/css/5.3f40a181.chunk.css"
+  },
+  {
+    "revision": "e66503c4022d4e8dd4d3",
+    "url": "/telegram-react/static/js/5.3c607d63.chunk.js"
+  },
+  {
+    "revision": "70bb8d17de7bac65c1d4",
+    "url": "/telegram-react/static/css/6.e689d479.chunk.css"
+  },
+  {
+    "revision": "70bb8d17de7bac65c1d4",
+    "url": "/telegram-react/static/js/6.043dd453.chunk.js"
+  },
+  {
+    "revision": "4979998bb21aca63da72",
+    "url": "/telegram-react/static/js/7.3722fe87.chunk.js"
+  },
+  {
+    "revision": "0a3c08e4ece092505997",
+    "url": "/telegram-react/static/js/8.284f5c0c.chunk.js"
+  },
+  {
+    "revision": "7a71629a5e4f7482b6320b103f1d543c",
+    "url": "/telegram-react/static/media/Manytabs_2x.7a71629a.png"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "5270d71cd78fd282a3736b0e6ae7f048",
+    "url": "/telegram-react/static/media/General_2x.5270d71c.png"
+  },
+  {
+    "revision": "f01cec9bccc80a678ed0ec04acecade2",
+    "url": "/telegram-react/static/media/IconsetW_2x.f01cec9b.png"
+  },
+  {
+    "revision": "64b843ee3346334d855852db7fcfc444",
+    "url": "/telegram-react/index.html"
+  }
+];
