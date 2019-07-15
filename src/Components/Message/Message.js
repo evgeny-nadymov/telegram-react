68674827fd2f3c10ebf48b92fa2639eb011cfbf3
evgeny-nadymov <johnnypmpu@gmail.com>
2019-07-15 14:25:13 +0300

    Open chat when channel avatar click

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -208,7 +208,7 @@ class Message extends Component {
     };
 
     handleSelectChat = chatId => {
-        openChat(chatId, true);
+        openChat(chatId, null, true);
     };
 
     handleSelection = () => {
