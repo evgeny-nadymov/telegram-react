df57c69a7b1e2252916c5dfa78d5c36eb1e43ea5
evgeny-nadymov <johnnypmpu@gmail.com>
2019-09-24 19:12:05 +0300

    Updates

--- precache-manifest.acb38c0d532c7789f109795314e06e85.js
+++ precache-manifest.acb38c0d532c7789f109795314e06e85.js
@@ -4,19 +4,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/e33e8791554f674a437f.worker.js"
   },
   {
-    "revision": "10bcf24cadbcc786f648572b5a5144e7",
+    "revision": "bfe4543ad953f71683056d8f7f917b56",
     "url": "/telegram-react/index.html"
   },
   {
-    "revision": "19b637f16f6104a044c1",
-    "url": "/telegram-react/static/css/3.c0c55157.chunk.css"
+    "revision": "aae7b6c6846a4cbdf957",
+    "url": "/telegram-react/static/css/3.c3b5e689.chunk.css"
   },
   {
     "revision": "4b77bada8a8f7ac4ece2",
     "url": "/telegram-react/static/css/5.1b8f2a33.chunk.css"
   },
   {
-    "revision": "d2dd506a45cf7e8cdc4e",
+    "revision": "4dc5e2ad41403c46e174",
     "url": "/telegram-react/static/css/main.cae0fa68.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/static/js/2.12a2c18d.chunk.js"
   },
   {
-    "revision": "19b637f16f6104a044c1",
-    "url": "/telegram-react/static/js/3.1640f9fb.chunk.js"
+    "revision": "aae7b6c6846a4cbdf957",
+    "url": "/telegram-react/static/js/3.41bb12e2.chunk.js"
   },
   {
     "revision": "4f05295bc292accabfe0",
@@ -40,12 +40,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/static/js/6.ba6bfa84.chunk.js"
   },
   {
-    "revision": "d2dd506a45cf7e8cdc4e",
-    "url": "/telegram-react/static/js/main.725e07d7.chunk.js"
+    "revision": "4dc5e2ad41403c46e174",
+    "url": "/telegram-react/static/js/main.4ec318f4.chunk.js"
   },
   {
-    "revision": "6c595f2d998a5a15e5df",
-    "url": "/telegram-react/static/js/runtime~main.a3639ea8.js"
+    "revision": "6d0d3e039b9e9180897c",
+    "url": "/telegram-react/static/js/runtime~main.f83b363b.js"
   },
   {
     "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
