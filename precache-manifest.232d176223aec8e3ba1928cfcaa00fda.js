bf4db046023a61028ff5227c10e196775376d359
evgeny-nadymov <johnnypmpu@gmail.com>
2019-11-27 09:53:20 +0300

    Updates

---
+++ precache-manifest.232d176223aec8e3ba1928cfcaa00fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01099d7802e9110d3fdda64efa107e1c",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "e3956bcebc87e5350a76",
+    "url": "/telegram-react/static/css/main.ce38a898.chunk.css"
+  },
+  {
+    "revision": "675007451518f3b53c8a",
+    "url": "/telegram-react/static/js/2.defd5be4.chunk.js"
+  },
+  {
+    "revision": "e3956bcebc87e5350a76",
+    "url": "/telegram-react/static/js/main.6b264686.chunk.js"
+  },
+  {
+    "revision": "fccf08bcbee56fb46383",
+    "url": "/telegram-react/static/js/runtime~main.1cc651bf.js"
+  }
+]);
