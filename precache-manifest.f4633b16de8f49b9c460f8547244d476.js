20752d4519d789763803902cc9609cbd7f6b1855
evgeny-nadymov <johnnypmpu@gmail.com>
2020-01-10 20:05:07 +0300

    Updates

---
+++ precache-manifest.f4633b16de8f49b9c460f8547244d476.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138a6e07e06f336198ccfcb3a873f341",
+    "url": "/telegram-react/483576f3911849b9af97.worker.js"
+  },
+  {
+    "revision": "94f81dc17aec09ba23cb94d03a78c12c",
+    "url": "/telegram-react/e33e8791554f674a437f.worker.js"
+  },
+  {
+    "revision": "d3c2d0b354c6d5ed3ba4c1d1f6acfb79",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "51db322c892cfa53c68e",
+    "url": "/telegram-react/static/css/3.8a6a59d3.chunk.css"
+  },
+  {
+    "revision": "226094bb616c93200cd0",
+    "url": "/telegram-react/static/css/main.da61066c.chunk.css"
+  },
+  {
+    "revision": "115f4a00a63342574f91",
+    "url": "/telegram-react/static/js/2.a2f6fde9.chunk.js"
+  },
+  {
+    "revision": "51db322c892cfa53c68e",
+    "url": "/telegram-react/static/js/3.8a8bdd03.chunk.js"
+  },
+  {
+    "revision": "70a91dfc962e0335e061",
+    "url": "/telegram-react/static/js/4.9c3c15f5.chunk.js"
+  },
+  {
+    "revision": "226094bb616c93200cd0",
+    "url": "/telegram-react/static/js/main.014b3868.chunk.js"
+  },
+  {
+    "revision": "ddfebbe4a45897790009",
+    "url": "/telegram-react/static/js/runtime~main.297bfd57.js"
+  },
+  {
+    "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
+    "url": "/telegram-react/static/media/Bubbles.5b7ac6f8.svg"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/telegram-react/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/telegram-react/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
