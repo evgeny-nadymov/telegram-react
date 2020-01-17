4a3d57e4a3c19c5ff5ea80e7682af56106db0114
evgeny-nadymov <johnnypmpu@gmail.com>
2020-01-17 12:46:17 +0300

    Updates

--- precache-manifest.e4c69f487acaa31033729b3c3c15759e.js
+++ precache-manifest.e4c69f487acaa31033729b3c3c15759e.js
@@ -8,36 +8,36 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/e33e8791554f674a437f.worker.js"
   },
   {
-    "revision": "601f08ce7f2fda216a9548be52bd7288",
+    "revision": "9c570ab60483427774981bc731c1a9db",
     "url": "/telegram-react/index.html"
   },
   {
-    "revision": "e9172a9bfdce28cb4af4",
+    "revision": "0308a719120a0251e259",
     "url": "/telegram-react/static/css/3.8a6a59d3.chunk.css"
   },
   {
-    "revision": "16ca5b7ea3bd581edb2f",
-    "url": "/telegram-react/static/css/main.70f6e626.chunk.css"
+    "revision": "f564e7e6d68b984fc827",
+    "url": "/telegram-react/static/css/main.c567be8e.chunk.css"
   },
   {
-    "revision": "641d7abc03fc2ad562f2",
-    "url": "/telegram-react/static/js/2.175a130f.chunk.js"
+    "revision": "0cf8d4d234891ebb8747",
+    "url": "/telegram-react/static/js/2.4f17cd8d.chunk.js"
   },
   {
-    "revision": "e9172a9bfdce28cb4af4",
-    "url": "/telegram-react/static/js/3.a7ee0257.chunk.js"
+    "revision": "0308a719120a0251e259",
+    "url": "/telegram-react/static/js/3.8cfb7b8d.chunk.js"
   },
   {
-    "revision": "6733e1f2cfd568e2a89d",
-    "url": "/telegram-react/static/js/4.9e858b5a.chunk.js"
+    "revision": "f433d565f17f9b433b4f",
+    "url": "/telegram-react/static/js/4.8d34cae9.chunk.js"
   },
   {
-    "revision": "16ca5b7ea3bd581edb2f",
-    "url": "/telegram-react/static/js/main.66079e37.chunk.js"
+    "revision": "f564e7e6d68b984fc827",
+    "url": "/telegram-react/static/js/main.403535b0.chunk.js"
   },
   {
-    "revision": "ba595b80900d34f5f823",
-    "url": "/telegram-react/static/js/runtime~main.ff7dfc5b.js"
+    "revision": "de0de8d68e8ee2783fbd",
+    "url": "/telegram-react/static/js/runtime~main.5bc96f85.js"
   },
   {
     "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
