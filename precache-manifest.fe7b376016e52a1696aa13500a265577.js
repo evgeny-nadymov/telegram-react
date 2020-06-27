dbd0e20462b99245cc831be56a5015de4e7c29df
evgeny-nadymov <johnnypmpu@gmail.com>
2020-06-27 07:08:00 +0300

    Updates

---
+++ precache-manifest.fe7b376016e52a1696aa13500a265577.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f81dc17aec09ba23cb94d03a78c12c",
+    "url": "/telegram-react/e33e8791554f674a437f.worker.js"
+  },
+  {
+    "revision": "48c28740eeb169dc83e0ef2dac7d9b90",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "f56e9d7642ff4df36960",
+    "url": "/telegram-react/static/css/0.885200b4.chunk.css"
+  },
+  {
+    "revision": "c2864875cb897875ec1c",
+    "url": "/telegram-react/static/css/4.d8fc5e7c.chunk.css"
+  },
+  {
+    "revision": "c913c9813d81d73f9d17",
+    "url": "/telegram-react/static/css/6.6ede3073.chunk.css"
+  },
+  {
+    "revision": "58321a6a2a7c9c341acb",
+    "url": "/telegram-react/static/css/8.3015292a.chunk.css"
+  },
+  {
+    "revision": "451c20746c6d2349337c",
+    "url": "/telegram-react/static/css/main.decd9524.chunk.css"
+  },
+  {
+    "revision": "f56e9d7642ff4df36960",
+    "url": "/telegram-react/static/js/0.9471dfec.chunk.js"
+  },
+  {
+    "revision": "2abfe301bfde54e8a2c8",
+    "url": "/telegram-react/static/js/1.aed744c6.chunk.js"
+  },
+  {
+    "revision": "c2864875cb897875ec1c",
+    "url": "/telegram-react/static/js/4.8f1bc539.chunk.js"
+  },
+  {
+    "revision": "045e5ac45fde2a8b6cdb",
+    "url": "/telegram-react/static/js/5.79337bde.chunk.js"
+  },
+  {
+    "revision": "c913c9813d81d73f9d17",
+    "url": "/telegram-react/static/js/6.e8642ff6.chunk.js"
+  },
+  {
+    "revision": "3cf60747fb800dcc4208",
+    "url": "/telegram-react/static/js/7.ea707708.chunk.js"
+  },
+  {
+    "revision": "58321a6a2a7c9c341acb",
+    "url": "/telegram-react/static/js/8.ecba072c.chunk.js"
+  },
+  {
+    "revision": "451c20746c6d2349337c",
+    "url": "/telegram-react/static/js/main.a4bbfbeb.chunk.js"
+  },
+  {
+    "revision": "d8cbbf9660d435f31bd1",
+    "url": "/telegram-react/static/js/runtime~main.eec5bcad.js"
+  },
+  {
+    "revision": "9b7ea631ddb3103beee05cec006af20e",
+    "url": "/telegram-react/static/media/bg.9b7ea631.jpg"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/telegram-react/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/telegram-react/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
--- tdweb.js
+++ tdweb.js
@@ -2429,7 +2429,7 @@ function () {
     key: "onResponse",
     value: function onResponse(response) {
       logger.debug('receive from worker: ', JSON.parse(JSON.stringify(response, function (key, value) {
-        if (key === 'arr') {
+        if (key === 'arr' || key == 'data') {
           return undefined;
         }
 
@@ -3074,7 +3074,7 @@ function () {
             switch (_context7.prev = _context7.next) {
               case 0:
                 if (!(!info.arr && !info.idb_key && info.file.local.path)) {
-                  _context7.next = 24;
+                  _context7.next = 22;
                   break;
                 }
 
@@ -3088,27 +3088,26 @@ function () {
 
               case 4:
                 count = _context7.sent;
-                logger.error(count, size);
 
                 if (size) {
-                  _context7.next = 10;
+                  _context7.next = 9;
                   break;
                 }
 
                 size = count.count;
-                _context7.next = 12;
+                _context7.next = 11;
                 break;
 
-              case 10:
+              case 9:
                 if (!(size > count.count)) {
-                  _context7.next = 12;
+                  _context7.next = 11;
                   break;
                 }
 
                 throw new Error('File not loaded yet');
 
-              case 12:
-                _context7.next = 14;
+              case 11:
+                _context7.next = 13;
                 return this.client.sendInternal({
                   '@type': 'readFilePart',
                   path: info.file.local.path,
@@ -3116,23 +3115,23 @@ function () {
                   count: size
                 });
 
-              case 14:
+              case 13:
                 _res = _context7.sent;
                 _res.data = new Blob([_res.data]);
-                _res.transaction_id = -2;
-                logger.error(_res);
+                _res.transaction_id = -2; //log.error(res);
+
                 return _context7.abrupt("return", _res);
 
-              case 21:
-                _context7.prev = 21;
+              case 19:
+                _context7.prev = 19;
                 _context7.t0 = _context7["catch"](1);
                 logger.info('readFilePart failed', info, offset, size, _context7.t0);
 
-              case 24:
-                _context7.next = 26;
+              case 22:
+                _context7.next = 24;
                 return this.doLoadFull(info);
 
-              case 26:
+              case 24:
                 res = _context7.sent;
                 // return slice(size, offset + size)
                 data_size = res.data.size;
@@ -3148,12 +3147,12 @@ function () {
                 res.data = res.data.slice(offset, offset + size);
                 return _context7.abrupt("return", res);
 
-              case 32:
+              case 30:
               case "end":
                 return _context7.stop();
             }
           }
-        }, _callee7, this, [[1, 21]]);
+        }, _callee7, this, [[1, 19]]);
       }));
 
       function doLoad(_x5, _x6, _x7) {
