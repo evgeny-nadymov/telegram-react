06a5c513365feb2f64fbcbbd0cd261381149275d
evgeny-nadymov <johnnypmpu@gmail.com>
2020-11-07 02:27:00 +0300

    New unpin icon

---
+++ src/Assets/Icons/PinOff.js
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (c) 2018-present, Evgeny Nadymov
+ *
+ * This source code is licensed under the GPL v.3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import createSvgIcon from '@material-ui/core/esm/utils/createSvgIcon';
+
+export default createSvgIcon(
+    <path d='M8,6.2V4H7V2H17V4H16V12L18,14V16H17.8L14,12.2V4H10V8.2L8,6.2M20,20.7L18.7,22L12.8,16.1V22H11.2V16H6V14L8,12V11.3L2,5.3L3.3,4L20,20.7M8.8,14H10.6L9.7,13.1L8.8,14Z' />,
+    'PinOff'
+);
