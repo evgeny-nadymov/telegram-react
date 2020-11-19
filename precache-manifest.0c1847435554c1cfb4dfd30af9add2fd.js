cc0ad5278d6c24d66466ed517b52ebaa41388ab4
evgeny-nadymov <johnnypmpu@gmail.com>
2020-11-19 20:55:25 +0300

    Updates

--- precache-manifest.0c1847435554c1cfb4dfd30af9add2fd.js
+++ precache-manifest.0c1847435554c1cfb4dfd30af9add2fd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c2a00158dce92232c72900a62c2ee72",
+    "revision": "2706a109d4ae82171ae52e027832da37",
     "url": "/telegram-react/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/static/css/0.52c23fd6.chunk.css"
   },
   {
-    "revision": "c2211589cf6de9c4676b",
-    "url": "/telegram-react/static/css/3.0b390eff.chunk.css"
+    "revision": "b4af10c31bdc95a288c3",
+    "url": "/telegram-react/static/css/3.ab969522.chunk.css"
   },
   {
     "revision": "df85f193a9304f36ca59",
@@ -28,7 +28,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/static/js/0.d0c698bb.chunk.js"
   },
   {
-    "revision": "c2211589cf6de9c4676b",
+    "revision": "b4af10c31bdc95a288c3",
     "url": "/telegram-react/static/js/3.5acbdca2.chunk.js"
   },
   {
@@ -52,8 +52,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/telegram-react/static/js/main.1808f81f.chunk.js"
   },
   {
-    "revision": "b3c4916d534200417bdf",
-    "url": "/telegram-react/static/js/runtime~main.ced67d88.js"
+    "revision": "f19f34c55ac1d6800648",
+    "url": "/telegram-react/static/js/runtime~main.5f98c57a.js"
   },
   {
     "revision": "cbb4eead04871188de6ce488808e4237",
