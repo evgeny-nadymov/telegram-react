29402464895cccf1e76a2dfbe3178be9f2c6ae63
evgeny-nadymov <johnnypmpu@gmail.com>
2021-04-20 01:44:11 +0300

    Updates

---
+++ precache-manifest.09a1ee4c25a0c67da779a061959e8f04.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b2f64a15042c7fc6fe476d6ec8be55",
+    "url": "/telegram-react/index.html"
+  },
+  {
+    "revision": "da46489ddc0b8e9c6934",
+    "url": "/telegram-react/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "80eddc8233535e8ee1ab",
+    "url": "/telegram-react/static/css/4.5b4eed0d.chunk.css"
+  },
+  {
+    "revision": "c459c108cbc526e7d0a0",
+    "url": "/telegram-react/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "f52bb71211b64c766ffb",
+    "url": "/telegram-react/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "f50f52bcaec9f78a7b76",
+    "url": "/telegram-react/static/css/main.162c7196.chunk.css"
+  },
+  {
+    "revision": "da46489ddc0b8e9c6934",
+    "url": "/telegram-react/static/js/0.fefb0dc8.chunk.js"
+  },
+  {
+    "revision": "63865af3649be3ce6d7b",
+    "url": "/telegram-react/static/js/3.7b0b54c4.chunk.js"
+  },
+  {
+    "revision": "80eddc8233535e8ee1ab",
+    "url": "/telegram-react/static/js/4.5cb3b09f.chunk.js"
+  },
+  {
+    "revision": "c459c108cbc526e7d0a0",
+    "url": "/telegram-react/static/js/5.130d91b5.chunk.js"
+  },
+  {
+    "revision": "f13252377fbd081a89e4",
+    "url": "/telegram-react/static/js/6.be74beef.chunk.js"
+  },
+  {
+    "revision": "f52bb71211b64c766ffb",
+    "url": "/telegram-react/static/js/7.d1b0b459.chunk.js"
+  },
+  {
+    "revision": "f50f52bcaec9f78a7b76",
+    "url": "/telegram-react/static/js/main.5c30d902.chunk.js"
+  },
+  {
+    "revision": "db0a2b21056c33c81c59",
+    "url": "/telegram-react/static/js/runtime~main.5aabc11a.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/telegram-react/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram-react/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-react/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-react/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
